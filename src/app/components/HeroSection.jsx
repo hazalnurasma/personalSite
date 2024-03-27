e5f0de37eb435b5d99d4de8e2bbc739392b3be90
hazalnurasma <asmahazalnur@gmail.com.tr>
2024-03-27 21:10:37 +0300

    first component is building.

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center">
+                <h1 className="mb-4 text-4xl lg:text-6xl font-extrabold">
+                    Hello, I`m Hazal.
+                </h1>
+
+                <p className="text-[#ADB7BE] text-lg lg:text-xl">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, nam!
+                </p>
+            </div>
+            <div className="col-span-5">
+                <img></img>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection

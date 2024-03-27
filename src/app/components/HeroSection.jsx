4b6049db6604851b5e4a095137df001dc29eeb96
hazalnurasma <asmahazalnur@gmail.com.tr>
2024-03-27 23:34:44 +0300

    hero section components is done.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,8 @@
+"use client"; // unhandled runtime erro about useRef -> and this works!?
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const HeroSection = () => {
   return (
@@ -7,20 +10,37 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl  lg:text-6xl font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-green-600 ">
                       Hello, I`m{" "}  
                     </span>
+                    <br></br>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Front-End Developer',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Web Developer',
+                            1000,
+                            'Data Scientist Candidate',
+                            1000,
+                            'Software Engineer',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
                 </h1>
 
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 mr-2 lg:text-xl ">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis tenetur commodi minima animi nemo sed in quae cumque, voluptatem accusamus!
                 </p>
                 <div>
-                    <button className='text-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-500 px-6 py-3 w-full sm:w-fit rounded-full mr-4'>
+                    <button className='text-white bg-gradient-to-br from-yellow-500 via-orange-500 to-green-500 px-6 py-3 w-full sm:w-fit rounded-full mr-4'>
                         Hire Me
                     </button>
-                    <button className='px-6 py-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-gray-500 text-white border border-white mt-3'>
-                        Download CV
+                    <button className='px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-yellow-500 via-orange-500 to-green-500 hover:bg-gray-500 text-white mt-3'>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
                     </button>
                 </div>
             </div>

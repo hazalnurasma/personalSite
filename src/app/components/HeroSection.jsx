f33db6df7e7e73e7a52821a2fa11aa13535a8702
hazalnurasma <asmahazalnur@gmail.com.tr>
2024-03-27 22:54:35 +0300

    hero section is almost done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const HeroSection = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center">
-                <h1 className="mb-4 text-4xl lg:text-6xl font-extrabold">
-                    Hello, I`m Hazal.
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl  lg:text-6xl font-extrabold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+                      Hello, I`m{" "}  
+                    </span>
                 </h1>
 
-                <p className="text-[#ADB7BE] text-lg lg:text-xl">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, nam!
+                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 mr-2 lg:text-xl ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis tenetur commodi minima animi nemo sed in quae cumque, voluptatem accusamus!
                 </p>
+                <div>
+                    <button className='text-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-500 px-6 py-3 w-full sm:w-fit rounded-full mr-4'>
+                        Hire Me
+                    </button>
+                    <button className='px-6 py-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-gray-500 text-white border border-white mt-3'>
+                        Download CV
+                    </button>
+                </div>
             </div>
-            <div className="col-span-5">
-                <img>Gonna add</img>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image 
+                    src="/images/hero-image.png"
+                    alt='hero image'
+                    className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 mr-4'
+                    width={300}
+                    height={300}
+                    />
+                </div>
+
             </div>
         </div>
     </section>
